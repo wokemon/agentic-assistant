d6858d4c8d5c340e58f9da915f51f314c86dd720
Hieu Nghia Phan <hieunghiabeo@gmail.com>
2026-05-07 08:58:55 +0700

    Fix registry, hook into the agent

--- src/tools/registy.ts
+++ src/tools/registy.ts
@@ -1,5 +1,7 @@
 import { listFiles } from "./listFiles";
+import { readFiles } from "./readFiles";
 
 export const tools: Record<string, Function> = {
   list_files: listFiles,
+  read_files: readFiles,
 };

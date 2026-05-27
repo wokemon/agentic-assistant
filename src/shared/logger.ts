56e8aaa885bcab0689b45eb0c59d36cc3663957b
Hieu Nghia Phan <hieunghiabeo@gmail.com>
2026-05-27 16:49:07 +0700

    Fix logger time issue

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -2,13 +2,12 @@ import pino from "pino";
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-
   transport: {
     target: "pino-pretty",
     options: {
-      colorize: true,
-      translateTime: "HH:MM:ss",
+      translateTime: "SYS:HH:MM:ss",
       ignore: "pid,hostname",
+      colorize: true,
     },
   },
 });

247100905bf9f0656ee13a439a8235694fefc242
Hieu Nghia Phan <hieunghiabeo@gmail.com>
2026-05-27 16:19:09 +0700

    Fix registry.ts

--- src/tools/registry.ts
+++ src/tools/registry.ts
@@ -3,11 +3,11 @@ import { ToolDefinition } from "../shared/types";
 import { listFilesTool } from "./filesystem/listFiles";
 import { readFilesTool } from "./filesystem/readFiles";
 import { writeFilesTool } from "./filesystem/writeFiles";
-import { executeTerminalCommand } from "./filesystem/terminals";
+import { terminalTool } from "./filesystem/terminals";
 
 export const tools: Record<string, ToolDefinition<any>> = {
   [listFilesTool.name]: listFilesTool,
   [readFilesTool.name]: readFilesTool,
   [writeFilesTool.name]: writeFilesTool,
-  [executeTerminalCommand.name]: executeTerminalCommand,
+  [terminalTool.name]: terminalTool,
 };
